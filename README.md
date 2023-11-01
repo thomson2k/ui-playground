c8e9f8c3a1618786694a0a6b5c8b9f52156c7366
Tomasz Watroba <w.t.tomasz@gmail.com>
2023-11-01 12:48:51 +0100

    feat: add shadcn

--- README.md
+++ README.md
@@ -1,87 +1,22 @@
+# next-template
 
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+A Next.js 13 template for building apps with Radix UI and Tailwind CSS.
 
-  
-
-## Getting Started
-
-  
-
-First, run the development server:
-
-  
+## Usage
 
 ```bash
-
-npm  run  dev
-
-# or
-
-yarn  dev
-
-# or
-
-pnpm  dev
-
-# or
-
-bun  dev
-
+npx create-next-app -e https://github.com/shadcn/next-template
 ```
 
-  
+## Features
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- Next.js 13 App Directory
+- Radix UI Primitives
+- Tailwind CSS
+- Icons from [Lucide](https://lucide.dev)
+- Dark mode with `next-themes`
+- Tailwind CSS class sorting, merging and linting.
 
-  
+## License
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-  
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-  
-
-# Comparison
-
-  
-
-## Chakra UI
-1.  Offers a more customizable theme and the ability to create custom variants and styles
-2.  Provides a well-organized theming system
-3.  Known for its mature and well-established library
-4.  Offers good default styling but may require more configuration to meet specific design needs
-5.  Has good established community
-  
-
-**Documentation**: Well written documentation with many examples. Additionaly we can find many pre-build templates such as: https://chakra-templates.dev/navigation/navbar
-
-**Cons**:
-
-- Chakra UI works somewhat fine with SSR with the old /pages, but it doesn't work yet with the new /app directory to use it we can add 'use client' on top file but then they'll be CSR not SSR 
-**TLDR:** Chakra doesn’t support SSR.
-
-**Pros**:
-
-- big community
-
-- components
-
-
-## Mantine
-1.  Offers a wide range of components and has more quantity of components.
-2.  Provides cleaner and more straightforward documentation
-3.  Defaults in Mantine require fewer overrides and are considered better out of the box
-4.  Components are designed to work efficiently without needing extensive configuration
-5.  May have more efficient handling of responsive design
-
-**Documentation:** Well written contains many example. A lot of components, hooks etc. Basically have similar experience to Chakra UI. 
-
-**Cons:**
-- some things doesn't works on server side
-- configuratrion api is a bit messy, less elegant I'd say bad for scaling
-
-**Pros:**
-
-- contains many ready to go components - https://ui.mantine.dev/
+Licensed under the [MIT license](https://github.com/shadcn/ui/blob/main/LICENSE.md).
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,43 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from "next/link"
 
-export default function Home() {
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import Table from "@/components/Table"
+import Input from "@/components/input"
+
+export default function IndexPage() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Beautifully designed components <br className="hidden sm:inline" />
+          built with Radix UI and Tailwind CSS.
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Accessible and customizable components that you can copy and paste
+          into your apps. Free. Open Source. And Next.js 13 Ready.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants()}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          GitHub
+        </Link>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Table />
+      <Input />
+    </section>
   )
 }
